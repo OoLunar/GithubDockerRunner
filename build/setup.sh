5d678378bc75e241b2fc6297d098defcbf02a420
Lunar Starstrum <lunar@forsaken-borders.net>
2023-01-09 13:16:08 -0600

    Multi-arch support

--- build/setup.sh
+++ build/setup.sh
@@ -4,17 +4,33 @@
 
 # Define the setup function
 setup() {
+    # Determine the CPU architecture
+  case "$(uname -m)" in
+    x86_64)
+      arch=x64
+      sha256=ed5bf2799c1ef7b2dd607df66e6b676dff8c44fb359c6fedc9ebf7db53339f0c
+      ;;
+    armv7l)
+      arch=arm
+      sha256=e3b35299483009fedfa55e2b27c371e14a113f40da32df886d846591f14b7873
+      ;;
+    aarch64)
+      arch=arm64
+      sha256=804693a178db3265eb43e09c3b4e67ef28f6d64133778b38d66dcffd2f21057d
+      ;;
+  esac
+
   # Download the Actions Runner package
-  curl -o actions-runner-linux-x64-2.299.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-x64-2.299.1.tar.gz
+  curl -o github-runner.tar.gz -L "https://github.com/actions/runner/releases/download/v2.300.2/actions-runner-linux-${arch}-2.300.2.tar.gz"
 
   # Check the SHA256 checksum of the package
-  echo "147c14700c6cb997421b9a239c012197f11ea9854cd901ee88ead6fe73a72c74  ./actions-runner-linux-x64-2.299.1.tar.gz" | sha256sum -c
+  echo "${sha256}  ./github-runner.tar.gz" | sha256sum -c
 
   # Extract the package
-  tar xzf ./actions-runner-linux-x64-2.299.1.tar.gz
+  tar xzf ./github-runner.tar.gz
 
   # Remove the package file
-  rm ./actions-runner-linux-x64-2.299.1.tar.gz
+  rm ./github-runner.tar.gz
 
   # Run the setup script
   su runner -c "./config.sh --url $GITHUB_REPOSITORY --token $GITHUB_TOKEN --unattended --replace --disableupdate"
