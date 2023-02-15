36e2809c712f7499f9a64fbfac9dc1489f0caaa2
Lunar Starstrum <lunar@forsaken-borders.net>
2023-02-15 12:32:39 -0600

    Update runner and switch to alpine

--- build/setup.sh
+++ build/setup.sh
@@ -17,20 +17,20 @@ setup() {
   case "$(uname -m)" in
     x86_64)
       arch=x64
-      sha256=ed5bf2799c1ef7b2dd607df66e6b676dff8c44fb359c6fedc9ebf7db53339f0c
+      sha256=15230bb2cf6af00e14ff294f85d2b6f07d6e856d229a1695cbe6290a16109995
       ;;
     armv7l)
       arch=arm
-      sha256=e3b35299483009fedfa55e2b27c371e14a113f40da32df886d846591f14b7873
+      sha256=8f70778a11ff9540886d97601c18a95a7ed388159af49c2c1c6f88eff4a00794
       ;;
     aarch64)
       arch=arm64
-      sha256=804693a178db3265eb43e09c3b4e67ef28f6d64133778b38d66dcffd2f21057d
+      sha256=885f02222f6e54682020dbb8beedf8226b039ec706ce3f3ea4b4ee626de014c3
       ;;
   esac
 
   # Download the Actions Runner package
-  curl -o github-runner.tar.gz -L "https://github.com/actions/runner/releases/download/v2.300.2/actions-runner-linux-${arch}-2.300.2.tar.gz"
+  curl -o github-runner.tar.gz -L "https://github.com/actions/runner/releases/download/v2.302.1/actions-runner-linux-${arch}-2.302.1.tar.gz"
 
   # Check the SHA256 checksum of the package
   echo "${sha256}  ./github-runner.tar.gz" | sha256sum -c
